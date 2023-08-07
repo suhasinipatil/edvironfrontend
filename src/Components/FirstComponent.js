d2cf63882638b2ff1c6e3442ca5084747dbdb244
suhasinipatil <suhasinibpatil09@gmail.com>
2023-08-07 16:22:51 +0530

    added the defaulters api

--- src/Components/FirstComponent.js
+++ src/Components/FirstComponent.js
@@ -1,6 +1,64 @@
 import MonetizationOnTwoToneIcon from '@mui/icons-material/MonetizationOnTwoTone';
+import { createContext, useContext, useState, useEffect } from "react";
 
 const FirstComponent = () => {
+    const schoolName = 'DAV Public School';
+    const [defaulter, setDefaulter] = useState(0);
+
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        const fetchData = async () => {
+          try {
+            const response = await fetch('http://localhost:8080/students/' + schoolName, {
+              method: 'GET',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+            });
+    
+            if (!response.ok) {
+              throw new Error('Network response was not ok');
+            }
+    
+            const data = await response.json();
+            setCount(data);
+            console.log(data);
+          } catch (error) {
+            console.error('Error fetching data:', error);
+            setCount(null);
+          }
+        };
+            
+        fetchData();
+    }, [schoolName]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+          try {
+            const response = await fetch('http://localhost:8080/students/defaulters/' + schoolName, {
+              method: 'GET',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+            });
+    
+            if (!response.ok) {
+              throw new Error('Network response was not ok');
+            }
+    
+            const data = await response.json();
+            setDefaulter(data);
+            console.log(data);
+          } catch (error) {
+            console.error('Error fetching data:', error);
+            setDefaulter(null);
+          }
+        };
+            
+        fetchData();
+    }, [schoolName]);
+    
   return (
     <div className="first-component">
         <table>
@@ -25,7 +83,7 @@ const FirstComponent = () => {
                 </th>
                 <th className="three-col">
                     <h5 className='headingStyle'>Defaulters</h5>
-                    <p>11/1049</p>
+                    <p>{defaulter}/{count}</p>
                     <p className='subParaStyle'>11% in last 30 days</p>
                 </th>
             </tr>
--- src/Components/SecondLevel.js
+++ src/Components/SecondLevel.js
@@ -3,6 +3,7 @@ import { createContext, useContext, useState, useEffect } from "react";
 const SecondLevel = () => {
     const schoolName = 'DAV Public School';
     const [count, setCount] = useState(0);
+    const [section, setSection] = useState(0);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -30,6 +31,32 @@ const SecondLevel = () => {
         fetchData();
     }, [schoolName]);
 
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const response = await fetch('http://localhost:8080/sections/' + schoolName, {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          });
+  
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+  
+          const data = await response.json();
+          setSection(data);
+          console.log(data);
+        } catch (error) {
+          console.error('Error fetching data:', error);
+          setCount(null);
+        }
+      };
+          
+      fetchData();
+  }, [schoolName]);
+
     return (
         <div className="second-Level">
             <div className="student-component">
@@ -38,7 +65,7 @@ const SecondLevel = () => {
             </div>
             <div className="section-component">
                 <h4 className='headingStyle'>Sections</h4>
-                <h2>18</h2>
+                <h2>{section}</h2>
                 <p>in 12 classes</p>
             </div>
             <div className="collection-component">
